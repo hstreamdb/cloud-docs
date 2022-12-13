a6cdf212aee793c08002daeb11149cf70a216353
Yue Yang <g1enyy0ung@gmail.com>
2022-12-13 12:19:09 +0800

    chore: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,56 @@
+import { defineUserConfig, defaultTheme } from 'vuepress'
+
+export default defineUserConfig({
+  title: 'HStream Cloud',
+  theme: defaultTheme({
+    logo: '/logo.svg',
+    sidebar: [
+      {
+        text: 'Introduction',
+        children: [
+          { text: 'What is HStream Cloud', link: '/introduction/what-is-hstream-cloud' },
+          { text: 'Architecture', link: '/introduction/architecture' },
+        ],
+      },
+      {
+        text: 'Getting Started',
+        children: [
+          { text: 'Apply for a Trial', link: '/getting-started/apply-for-a-trial' },
+          { text: 'Try out HStream Cloud', link: '/getting-started/try-out-hstream-cloud' },
+        ],
+      },
+      {
+        text: 'Manage Streams',
+        children: [
+          { text: 'Create a Stream', link: '/manage-streams/create-a-stream' },
+          { text: 'Write Records to a Stream', link: '/manage-streams/write-records-to-a-stream' },
+          { text: 'Stream Details', link: '/manage-streams/stream-details' },
+          { text: 'Delete a Stream', link: '/manage-streams/delete-a-stream' },
+        ],
+      },
+      {
+        text: 'Manage Subscriptions',
+        children: [
+          { text: 'Create a Subscription', link: '/manage-subscriptions/create-a-subscription' },
+          {
+            text: 'Consume Records from a Subscription',
+            link: '/manage-subscriptions/consume-records-from-a-subscription',
+          },
+          { text: 'Subscription Details', link: '/manage-subscriptions/subscription-details' },
+          { text: 'Delete a Subscription', link: '/manage-subscriptions/delete-a-subscription' },
+        ],
+      },
+      {
+        text: 'Data Query',
+        children: [{ text: 'Query Records', link: '/data-query/query-records' }],
+      },
+      {
+        text: 'API',
+        children: [
+          { text: 'API Overview', link: '/api/overview' },
+          { text: 'API Reference', link: '/api/reference' },
+        ],
+      },
+    ],
+  }),
+})
